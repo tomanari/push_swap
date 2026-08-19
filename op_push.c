5a7a51b34dd2445f1e6a3fb0e86961125167de04
tomanari <tomanari.lol@gmail.com>
2026-08-19 14:21:04 -0300

    op_push

--- op_push.c
+++ op_push.c
@@ -2,5 +2,26 @@
 
 void	ft_push(t_stack *from, t_stack *to)
 {
-	
+	int	temp;
+	int i;
+
+	if (!from || !to || from->size == 0)
+		return ;
+
+	temp = from->values[0];
+	i = 0;
+	while (i < from->size - 1)
+	{
+		from->values[i] = from->values[i + 1];
+		i++;
+	}
+	i = to->size;
+	while(i > 0)
+	{
+		to->values[i] = to->values[i - 1];
+		i--;
+	}
+	to->size++;
+	to->values[0] = temp;
+	from->size--;
 }
